dc46ddf2493709d3d92478148055b53f688c607b
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-05 11:38:38 -0500

    added relations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ApplicationRecord
+  has_many :user_recipes, :recipe_ingredients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :user_recipies, :user_ingredients
+  has_many :ingredients, through: :user_ingredients
+  has_many :recipes, through: :user_recipes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
