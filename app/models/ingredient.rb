43b3777028014311342cbf431aaf35bf89a816ae
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-05 12:07:47 -0500

    Validation done

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients, :user_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: "Ingredient already exists" }
 end
