01ff1a623c166335947e84e63023096e32721a6e
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-06 15:20:51 -0500

    Created a show and index view where we can see user's recipes ans click on a Link_to 'details'. I also comment the favoritor in the app file, because it was breacking the code. I added an 'ingredients' column to 'recipe' model because we forgot'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_163012) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_201424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_163012) do
     t.string "cooking_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ingredients"
   end
 
   create_table "user_ingredients", force: :cascade do |t|
