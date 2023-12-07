df81eac33e2d396d5a77eda23beaa9c06e7fccf6
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-07 12:38:59 -0500

    starting to display the search result in ingredient#index view

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,26 @@
+class IngredientsController < ApplicationController
+
+  def index
+    if params[:query].present?
+      @ingredients = Ingredient.search_ingredient_in_db(params[:query])
+    else
+      @ingredients = Ingredient.all
+    end
+    @vegetables = Ingredient.where(category: "vegetables")
+    @fruits = Ingredient.where(category: "fruits")
+    @starch = Ingredient.where(category: "starch")
+    @legumes = Ingredient.where(category: "legumes")
+    @flour = Ingredient.where(category: "flour")
+    @mushrooms = Ingredient.where(category: "mushrooms")
+    @spices = Ingredient.where(category: spices)
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+    # @ = Ingredient.where(category: )
+
+  end
+
+end
