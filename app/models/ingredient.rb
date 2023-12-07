71031e2acdcb16b2cd14024669706d7c1141fdf3
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-06 21:19:48 -0500

    created a search form and redered it in 'ingredient list'(user_ingredients#index)

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,12 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
 
   validates :name, presence: true, uniqueness: { case_sensitive: false, message: "Ingredient already exists" }
+
+  include PgSearch::Model
+  pg_search_scope :search_ingredient_in_db,
+                  against: [:name],
+                  using: {
+                    tsearch: { dictionary: "english",
+                              prefix: true } # <-- non-completed word will return something!
+                  }
 end
