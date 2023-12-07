4de4d3c0dfa84353447a043216d1cb2401e482e8
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-07 13:05:42 -0500

    Cloudinay and Active storage implementation in progress

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,14 @@
 class Recipe < ApplicationRecord
   # acts_as_favoritable
+  has_one_attached :image
   has_many :user_recipes, dependent: :destroy
   has_many :recipe_ingredients, dependent: :destroy
   validates :name, :instructions, presence: true
 
   include PgSearch::Model
   pg_search_scope :search_recipe,
-    against: [:name, :instructions, :name],
-    using: {
-      tsearch: { prefix: true, dictionary: "english", any_word: true }
-    }
+                  against: %i[name instructions name],
+                  using: {
+                    tsearch: { prefix: true, dictionary: "english", any_word: true }
+                  }
 end
