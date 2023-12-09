484e0b4f2595ccc1c9541efb069ebbbc3fdf2139
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-09 05:00:47 -0500

    recipes page almost done

--- app/javascript/controllers/search_repices_controller.js
+++ app/javascript/controllers/search_repices_controller.js
@@ -4,9 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "form", "input", "recipes" ]
   connect() {
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-    console.log(this.recipesTarget);
   }
 
   search(event) {
