80c25813be019473cdeb14d16bb896406f3bb81b
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-11 12:52:30 -0500

    saving

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,13 +5,17 @@ class RecipesController < ApplicationController
     ingredients_id = UserIngredient.where(user: current_user).pluck(:ingredient_id)
     # Without the pluck, I would only get an array of user_ingredients instances
 
-    # @ingredients_name = Ingredient.find(ingredients_id).pluck(:name).join(' ')
-    # @fridge_recipes = Recipe.search_recipes(@ingredients_name)
+    @ingredients_name = Ingredient.find(ingredients_id).pluck(:name).join(' ')
+    @fridge_recipes = Recipe.search_recipes(@ingredients_name) if params[:]
+
     @recipes = Recipe.search_recipe(params[:query]) if params[:query].present?
     # I want the params[:query] to be equal to the ingredients list
     # Put a button where you can 'Find recipe with what's left in the fridge'
     # This button should launch the search with the pluck array of ingredient's names.
     # So Recipe.search_recipe(pluck_result)
+
+    #  add a link to with params
+    # if htis params exists,
 
 
     respond_to do |format|
