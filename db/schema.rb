2d0fe128f9e68a23081bffed8772107271403711
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-11 12:16:30 -0500

    recipe_collection model and controller removed, user add to collection , user_recipe_collection table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_10_194506) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_11_171427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_10_194506) do
     t.index ["user_id"], name: "index_user_ingredients_on_user_id"
   end
 
+  create_table "user_recipe_collections", force: :cascade do |t|
+    t.bigint "collection_id", null: false
+    t.bigint "user_recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["collection_id"], name: "index_user_recipe_collections_on_collection_id"
+    t.index ["user_recipe_id"], name: "index_user_recipe_collections_on_user_recipe_id"
+  end
+
   create_table "user_recipes", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "recipe_id", null: false
@@ -127,6 +136,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_10_194506) do
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "user_ingredients", "ingredients"
   add_foreign_key "user_ingredients", "users"
+  add_foreign_key "user_recipe_collections", "collections"
+  add_foreign_key "user_recipe_collections", "user_recipes"
   add_foreign_key "user_recipes", "recipes"
   add_foreign_key "user_recipes", "users"
 end
--- test/controllers/recipe_collections_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class RecipeCollectionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
