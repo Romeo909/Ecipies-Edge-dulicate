8b22e17a388e0b52cef97d28f2bc649f90ed163e
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-11 12:49:46 -0500

    Trying to get ingredients name to then launch the search woth the ingredients'

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,19 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
+    # raise
+    ingredients_id = UserIngredient.where(user: current_user).pluck(:ingredient_id)
+    # Without the pluck, I would only get an array of user_ingredients instances
+
+    # @ingredients_name = Ingredient.find(ingredients_id).pluck(:name).join(' ')
+    # @fridge_recipes = Recipe.search_recipes(@ingredients_name)
     @recipes = Recipe.search_recipe(params[:query]) if params[:query].present?
+    # I want the params[:query] to be equal to the ingredients list
+    # Put a button where you can 'Find recipe with what's left in the fridge'
+    # This button should launch the search with the pluck array of ingredient's names.
+    # So Recipe.search_recipe(pluck_result)
+
+
     respond_to do |format|
       format.html
       format.text { render partial: 'list', locals: { recipes: @recipes }, formats: [:html] }
