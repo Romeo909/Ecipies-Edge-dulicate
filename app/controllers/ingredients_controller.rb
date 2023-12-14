e50b6424f21a42026bddc7a3456274731db9ebd0
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-14 17:16:53 -0500

    Home page is done

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,4 @@
 class IngredientsController < ApplicationController
-
   def index
     if params[:query].present?
       @ingredients = Ingredient.search_ingredient_in_db(params[:query])
@@ -7,10 +6,10 @@ class IngredientsController < ApplicationController
       @ingredients = Ingredient.all
     end
     @user_ingredient_ids = UserIngredient.where(user: current_user).pluck(:ingredient_id)
-    ingredients = Ingredient.find(@user_ingredient_ids)
+    # ingredients = Ingredient.find(@user_ingredient_ids)
 
-    category_ids = ingredients.pluck(:category_id)
-    @categories = Category.where.not(id: category_ids)
+    # category_ids = ingredients.pluck(:category_id)
+    # @categories = Category.where.not(id: category_ids)
+    @categories = Category.all
   end
-
 end
