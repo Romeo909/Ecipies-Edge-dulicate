4142dd1aa53cc5971103dc762188577f14348c0f
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-14 20:52:56 -0500

    Add to favorite is completely fixed, user can add the same recipe to different categories, and when user delete a recipe from categorie folder it delete it in the other categories

--- app/controllers/user_recipe_collections_controller.rb
+++ app/controllers/user_recipe_collections_controller.rb
@@ -1,6 +1,13 @@
 class UserRecipeCollectionsController < ApplicationController
   def destroy
     @user_recipe_collection = UserRecipeCollection.find(params[:id])
+    all_default_collection = Collection.where(user: current_user, name: "All")[0]
+    if @user_recipe_collection.collection == all_default_collection
+      UserRecipeCollection.where(user_recipe: @user_recipe_collection.user_recipe).destroy_all
+    else
+      UserRecipeCollection.where(user_recipe: @user_recipe_collection.user_recipe,
+                                 collection: all_default_collection).destroy_all
+    end
     @user_recipe_collection.destroy
     redirect_to collection_path(@user_recipe_collection.collection)
   end
